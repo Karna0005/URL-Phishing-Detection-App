3370704b59e730e42ee8d37ae666fef53cb2191d
Karunakar S <106344460+Karna0005@users.noreply.github.com>
2026-08-03 19:31:40 +0530

    Update app.py

--- phishing-detection-streamlit-app-main/app.py
+++ phishing-detection-streamlit-app-main/app.py
@@ -386,12 +386,15 @@ def main():
     input_url = st.text_input(" Enter The URL With HTTP/HTTPS & WWW ")
 
     if st.button('Predict'):
+        with open("phishing.pkl", "rb") as file:
+            rfc = pickle.load(file)
+    
         # Extract features from the input URL
         fe = FeatureExtraction(input_url)
         features = fe.extract_features()
 
         # Load the trained Random Forest model
-        rfc = pickle.load(open("phishing.pkl", 'rb'))
+        #rfc = pickle.load(open("phishing.pkl", 'rb'))
 
         # Make prediction with the Random Forest model
         rfc_prediction = rfc.predict([features])[-1]  # Corrected indexing
